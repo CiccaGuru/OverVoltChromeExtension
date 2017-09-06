fd0e657b04350fbffac71635388a0b062654a911
CiccaGuru <robyciccarelli.rc@gmail.com>
2017-09-06 18:53:24 +0200

    Fix Amazon bug

--- event.js
+++ event.js
@@ -3,15 +3,25 @@ chrome.webRequest.onBeforeRequest.addListener(
      var url = details.url;
      var splitted = url.split("/");
      var urlNuovo = url;
+     counter = 0;
      switch(splitted[2])  {
        case "www.amazon.it":
-       if(url.indexOf("?tag=overVolt-21")<0){
-         var index = url.indexOf("/ref");
-         if(index > 0){
-          urlNuovo = url.slice(0, index) +  "?tag=overVolt-21";
-          console.log(urlNuovo);
-        }
+       len = urlNuovo.length;
+       tagIndex = urlNuovo.indexOf("tag=");
+       while(tagIndex > 0){
+         nextParameterIndex = urlNuovo.slice(tagIndex).indexOf("&");
+         if((nextParameterIndex+tagIndex+1>=len)||(nextParameterIndex <0)){
+           urlNuovo = urlNuovo.slice(0, tagIndex-1);
+         } else {
+            urlNuovo = urlNuovo.slice(0, tagIndex-1) + urlNuovo.slice(nextParameterIndex + tagIndex);
+         }
+         len = urlNuovo.length;
+         tagIndex =urlNuovo.indexOf("tag=");
        }
+
+       var separator = urlNuovo.indexOf("?")>0 ? "&" : "?";
+       urlNuovo += separator + "tag=overVolt-21";
+
       break;
        case "www.banggood.com":
        if(url.indexOf(".html?p=63091629786202015112")<0){
